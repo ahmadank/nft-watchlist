27a46fb96ac66600841eac35902491ae3c712ca2
ahmadank <ahmedandnq@hotmail.com>
2023-01-07 16:05:02 -0500

    Connected Supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import { supabase } from "./../lib/supabaseClient";
+
+async function data() {
+  let { data } = await supabase.from("collections").select();
+  console.log(data);
+}
+
+export default async function Home() {
+  await data();
   return (
     <>
       <title>Home</title>
