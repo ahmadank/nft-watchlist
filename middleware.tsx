b2278e1311ea9540623600b9160db2f873e99e07
ahmadank <ahmedandnq@hotmail.com>
2023-01-07 21:31:45 -0500

    moved middleware to use ts

--- middleware.tsx
+++ middleware.tsx
@@ -1,10 +1,12 @@
 import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function middleware(req) {
+export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
   const supabase = createMiddlewareSupabaseClient({ req, res });
+
   const {
     data: { session },
   } = await supabase.auth.getSession();
