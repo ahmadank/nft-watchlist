41b6d086275143700c6166ad4f2c66e17ecdd7ae
ahmadank <ahmedandnq@hotmail.com>
2023-01-20 16:25:14 -0500

    Some css fixes

--- app/[project]/page.tsx
+++ app/[project]/page.tsx
@@ -1,10 +1,22 @@
+import styles from "../styles/Header.module.css";
+import getData from "../../functions/getDataProject";
+
 type PageProps = {
   params: {
     project?: string;
   };
   searchParams?: unknown;
 };
-function page(props: PageProps) {
-  return <div>page</div>;
+async function page(props: PageProps) {
+  const data = await getData(props.params.project);
+  return (
+    <main>
+      <p style={{ textAlign: "center" }}>
+        Hey we are excited that you are trying to access more info, however we
+        are still building this site and are waiting for our API token. Thank
+        you for understanding
+      </p>
+    </main>
+  );
 }
 export default page;
